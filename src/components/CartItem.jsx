281f00dfac9ccfced8b36b3a5d161023e9a27ee9
MikhailWahib <mikhailwahib1@gmail.com>
2022-12-28 11:19:34 +0200

    Desktop image gallery, Mobile image gallery, interactive cart and full responsive

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import img from '../assets/images/image-product-1-thumbnail.jpg'
 import deleteIcon from '../assets/images/icon-delete.svg'
 
-const CartItem = ({ setProductCount }) => {
+const CartItem = ({ productCount, setProductCount }) => {
     return (
         <div className='cart-item'>
             <div>
@@ -11,7 +11,7 @@ const CartItem = ({ setProductCount }) => {
             <div className="cart-item-details">
                 <p>Fall Limited Edition Sneakers</p>
                 <div>
-                    <p>$125.00 x 3</p><span>$375.00</span>
+                    <p>$125.00 x {productCount}</p><span>${(125.00 * productCount).toFixed(2)}</span>
                 </div>
             </div>
             <button className="cart-delete-btn" onClick={() => setProductCount(0)}>
--- src/components/ProductImages.jsx
+++ src/components/ProductImages.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import img1 from '../assets/images/image-product-1.jpg'
 import img2 from '../assets/images/image-product-2.jpg'
 import img3 from '../assets/images/image-product-3.jpg'
@@ -10,22 +10,64 @@ import thumb4 from '../assets/images/image-product-4-thumbnail.jpg'
 import arrow from '../assets/images/icon-next.svg'
 
 const ProductImages = () => {
+
+    const imgs = [img1, img2, img3, img4];
+    const thumbnails = [thumb1, thumb2, thumb3, thumb4];
+
+    const [currentImg, setCurrentImg] = useState(imgs[0]);
+    const [currentImgIndex, setCurrentImgIndex] = useState(0);
+
+    const handleNextBtn = () => {
+        if (currentImgIndex > -1 && currentImgIndex < imgs.length - 1) {
+            setCurrentImgIndex(currentImgIndex + 1);
+        }
+    }
+
+    const handleBackBtn = () => {
+        if (currentImgIndex > 0 && currentImgIndex < imgs.length) {
+            setCurrentImgIndex(currentImgIndex - 1);
+        }
+    }
+
+    useEffect(() => {
+        setCurrentImg(imgs[currentImgIndex]);
+    }, [currentImgIndex])
+
     return (
         <div className='product-imgs-container'>
+
             {/* mobile arrows */}
             <div className="mobile-arrows-container">
-                <button className="arrow"><img src={arrow} className='back-arrow' /></button>
-                <button className="arrow"><img src={arrow} /></button>
+                <button
+                    className="arrow"
+                    onClick={handleBackBtn}
+                >
+                    <img src={arrow} className='back-arrow' />
+                </button>
+                <button
+                    className="arrow"
+                    onClick={handleNextBtn}
+                >
+                    <img src={arrow} />
+                </button>
             </div>
             {/* mobile arrows */}
+
             <div className="main-img-wrapper">
-                <img src={img1} alt="product image" className='main-img' />
+                <img src={currentImg} alt="product image" className='main-img' />
             </div>
             <div className="thumbnails-container">
-                <button className='thumbnail-btn'><img src={thumb1} alt="product thumbnail" /></button>
-                <button className='thumbnail-btn'><img src={thumb2} alt="product thumbnail" /></button>
-                <button className='thumbnail-btn'><img src={thumb3} alt="product thumbnail" /></button>
-                <button className='thumbnail-btn'><img src={thumb4} alt="product thumbnail" /></button>
+                {
+                    thumbnails.map((thumb, index) => {
+                        return <button
+                            className='thumbnail-btn'
+                            key={index}
+                            onClick={() => setCurrentImg(imgs[index])}
+                        >
+                            <img src={thumb} alt="product thumbnail" />
+                        </button>
+                    })
+                }
             </div>
         </div>
     )
